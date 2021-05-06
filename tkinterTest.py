b36ff2f1dd09ad391d84bfb85ce2a0248f7f3799
Rae <79872916+flora-pura@users.noreply.github.com>
2021-05-06 14:21:30 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -5,26 +5,33 @@ playlist = []
 
 def addSong():
     playlist.append(E1.get())
+    E1.delete(0, END)
 
 def printList():
     print(playlist)
 
+def exportPlaylist():
+    with open('playlist.txt', 'w') as f:
+        for song in playlist:
+            f.write("%s\n" % song)
+
 #this is a lable widget
 L1 = Label(top, text="Hello, world!")
-L1.grid(column= 0, row= 1)
+L1.grid(column = 0, row = 1)
 
 #this is a text entry widget
 E1 = Entry(top, bd = 5)
 E1.grid(column = 0, row = 2)
 
 #this is a button widget
-B1 = Button(text="Add to Playlist", bg = "light blue", command = addSong)
+B1 = Button(text=" + ", bg = "#B4E9FF", command = addSong)
 B1.grid(column = 1, row = 2)
 
-B2 = Button("Export List", bg = "green", command = printList)
-B2.grid(column=1, row =1)
+B2 = Button(text = "Print List", bg = "#D9B1FF", fg = "white", command = printList)
+B2.grid(column = 0, row = 3)
 
-
+B3 = Button(text = "Export List", bg = "#FFD4FE", command = exportPlaylist)
+B3.grid(column = 1, row = 3) 
 
 
 top.mainloop()
